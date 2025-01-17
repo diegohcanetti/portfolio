b0b9772e7341ccac79a5c71141eff84e28e88805
diegohcanetti <102698705+diegohcanetti@users.noreply.github.com>
2025-01-17 17:58:24 -0300

    Uploaded half of the model

---
+++ Public Transport and Establishments Service Status Monitoring - 2023 College Project/src/main/java/models/persistence/Repositorios/RepositorioGenerico.java
@@ -0,0 +1,36 @@
+package models.persistence.Repositorios;
+
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
+import models.persistence.EntityManagerSingleton;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+public abstract class RepositorioGenerico<T> implements WithSimplePersistenceUnit {
+    @PersistenceContext
+    private Class<T> entityClass;
+    private EntityManager entityManager;
+    public RepositorioGenerico(Class<T> entityClass) { this.entityClass = entityClass; }
+    public void agregar(T entity) {
+        entityManager = EntityManagerSingleton.getInstance();
+        entityManager.persist(entity);
+    }
+
+    public void agregarNuevo(T entity, EntityManager entityManager) {
+        entityManager.persist(entity);
+    }
+    public void eliminar(T entity) {
+        entityManager = EntityManagerSingleton.getInstance();
+        entityManager.remove(entity);
+    }
+
+    public T buscar(long id) {
+        entityManager = EntityManagerSingleton.getInstance();
+        return entityManager.find(entityClass, id);
+    }
+
+    public List<T> buscarTodos() {
+        entityManager = EntityManagerSingleton.getInstance();
+        return entityManager.createQuery("from " + entityClass.getSimpleName(), entityClass).getResultList(); }
+}
